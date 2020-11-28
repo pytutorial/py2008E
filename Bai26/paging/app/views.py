b9b9a09a84845952117d3eaf4389303f43f49aea
dttvn0010 <duongthanhtungvn01@gmail.com>
2020-11-28 21:37:52 +0700

    Update

--- Bai26/paging/app/views.py
+++ Bai26/paging/app/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
-
+from .models import Product
+import math
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    PAGE_SIZE = 10
+    name = request.GET.get('name', '')
+    productList = Product.objects.filter(name__icontains=name)
+
+    page = request.GET.get('page', '')
+    page = int(page) if page.isdigit() else 1
+    start = (page-1)*PAGE_SIZE
+    end = start + PAGE_SIZE
+
+    context  = {
+        'productList': productList[start:end], 
+        'name': name,
+        'page': page,
+        'num_page': math.ceil(len(productList)/PAGE_SIZE),
+    }
+
+    return render(request, 'index.html', context)
