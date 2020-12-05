f69d274adda9820f4594352a44352ed6286cac52
dttvn0010 <duongthanhtungvn01@gmail.com>
2020-12-05 09:32:00 -0500

    Update

--- project/app/urls_staff.py
+++ project/app/urls_staff.py
@@ -3,5 +3,8 @@ from django.urls import path
 from .views_staff import *
 
 urlpatterns = [
+    path('list-category', listCategory),
+    path('create-category', CategoryCreateView.as_view()),
+
     path('', listProduct),
 ]
--- project/app/views_staff.py
+++ project/app/views_staff.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from .models import *
+from django.views.generic.edit import CreateView, UpdateView
+
+class CategoryCreateView(CreateView):
+    model = Category
+    fields = '__all__'
+    template_name = 'staff/category/form.html'
+    success_url = '/staff/list-category'
+
+def listCategory(request): #127.0.0.1:8000/staff/list-category
+    categoryList = Category.objects.all()
+    context = {'categoryList': categoryList}
+    return render(request, 'staff/category/list.html', context)
 
 def listProduct(request):
     return render(request, 'staff/product/list.html')
